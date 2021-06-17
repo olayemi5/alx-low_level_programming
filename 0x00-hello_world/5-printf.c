ebdb770e345e8cff9aafc3bdeef334394dbf9c83
root <olayemistephen007@gmail.com>
2021-06-17 12:00:25 -0700

    Added new file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *main - Entry point of code
+ *
+ *Always "return (0), which means success!
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
