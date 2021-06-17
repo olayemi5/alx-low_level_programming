7f2ab0ed0dd8a0713ac3679c51c9a7ffdbaecb32
root <olayemistephen007@gmail.com>
2021-06-17 13:11:07 -0700

    Updated file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
- *main - Entry point of code
+/**
+ * main - Entry point of code
  *
- *Always "return (0), which means success!
+ * Return: Always 0 (success)
  */
 
 int main(void)
