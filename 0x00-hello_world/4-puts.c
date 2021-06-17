bfc27ea0c011b07b4619dc01bba1521dac421beb
root <olayemistephen007@gmail.com>
2021-06-17 11:57:12 -0700

    added file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,11 @@
 
 int main(void)
 {
+  /*
+ *main - entry point of code
+ *
+ *Always "return (0)", which means success!
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
