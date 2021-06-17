1852e8f624ea0d647456860d33d67a5210f379c2
root <olayemistephen007@gmail.com>
2021-06-17 11:51:39 -0700

    Added c file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *main - entry point of code
+ *
+ *Always "return (0)", which means success!
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
