61699119d8395508bd7d1382a83c978ec1d05f6f
root <olayemistephen007@gmail.com>
2021-07-12 12:38:18 -0700

    Added ne files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of pointers
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *(argv + 0));
+	return (0);
+}
